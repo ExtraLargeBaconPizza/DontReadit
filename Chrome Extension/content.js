c95a83dd3c0259278137a800bfff9af709eb0ee4
jim <jim@bitesquad.com>
2020-04-27 20:39:27 -0400

    make it less condense lol

--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -1,21 +1,31 @@
 (function()
 {
+    removeSideAds();
+    removePromotedAds();
+
     // need to check again any time a node is inserted to the page
     $(document).on('DOMNodeInserted', function(e) 
     {
-        // hide side ads
-        $('div[data-before-content="advertisement"]:visible').each(function() 
-        {
-            this.parentNode.parentNode.parentNode.style.display = "none";
-        });
-
-        // hide promoted ads
-        $("span:contains(promoted):visible").each(function() 
-        {
-            getParentNodeUntilNoClassThenHide(this);
-        });
+        removeSideAds();
+        removePromotedAds();
     });
 })();
+
+function removeSideAds()
+{
+    $('div[data-before-content="advertisement"]:visible').each(function() 
+    {
+        this.parentNode.parentNode.parentNode.style.display = "none";
+    });
+}
+
+function removePromotedAds()
+{
+    $("span:contains(promoted):visible").each(function() 
+    {
+        getParentNodeUntilNoClassThenHide(this);
+    });
+}
 
 // why don't you come and see me when you've got no class - Rodney Dangerfield
 function getParentNodeUntilNoClassThenHide(element)
